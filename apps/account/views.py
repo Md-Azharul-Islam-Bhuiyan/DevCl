e95ebb3f9d3b562f548e83c1d1c9e9c3dbcd1c73
Md Azharul Islam Bhuiyan <mdazharulislambhuiyaniiuc@gmail.com>
2024-06-30 07:26:32 +0600

    [UPDATED]: Account Views

--- apps/account/views.py
+++ apps/account/views.py
@@ -27,6 +27,7 @@ User = get_user_model()
 def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
     access = refresh.access_token
+    access['id'] = user.user_id
     access['username'] = user.username
     access['email'] = user.email
     access['role'] = user.role
